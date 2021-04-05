2a1c23c05e712b636c5c4e5a81362c6826d8715d
SaracensFan <matthewclifford@hotmail.co.uk>
2021-04-05 12:41:02 +0100

    all changes

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php
+    require 'header.php';
+?>
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col-sm-2">
+        </div>
+
+        <div class="col-sm-8 ">
+            <form action="javascript:void(0)" method="POST">
+                <label for="">Username</label>
+                <input type="text" name="" placeholder="Username"/><br>
+                <label for="">Password</label>
+                <input type="password" name="" placeholder="Password"/>
+                <input type="submit" name="login-submit" id="">
+            </form>
+        </div>
+
+        <div class="col-sm-2"></div>
+
+        </div>
+</div>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+    require 'header.php'
+?>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+    
+            <div class="col-sm-2 cont">
+                <ul>
+                    <li>Bodyshop</li>
+                    <li>Bodyshop ID:</li>
+                    <li>User</li>
+                    <li>User ID</li>
+                </ul>
+            </div>
+
+            <div class="col-sm-8">
+
+            </div>
+    
+            <div class="col-sm-2"></div>
+    
+            </div>
+    </div>
+</body>
+</html>
