8d98059580063f860aab6c3b742c6b2e1de2b164
SaracensFan <matthewclifford@hotmail.co.uk>
2021-04-07 21:47:13 +0100

    updates

--- onlinevda.php
+++ onlinevda.php
@@ -3,30 +3,75 @@
 ?>
 
 <div class="container-fluid">
-            <div class="row">
+    <div class="row">
 
-                <div class="col-sm-4"></div>
-                
-                <div class="col-sm-4">
-                    <form action="login.php" method="">
-                       
-                    <div class="form-group">
-                            <label for="name">Username</label>
-                            <input class="form-control" type="text" name="user" id="" placeholder="Username...">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Password</label>
-                            <input class="form-control" type="text" name="psw" id="" placeholder="Password...">
-                        </div>
-
-                        <button class="btn1 btn-sm">Submit</button>
-                    </form>
-                </div>
-
-                <div class="col-sm-4"></div>
-
+        <div class="col-xxl-4"></div>
+        
+        <div class="col-xxl-4">
+            <form action="javascript:void(0)" method="" class="vdaform">
+               
+            
+            <div class="vda-group">
+                <label for="name">Customer Suffix.</label>
+                <select class="form-select form-select-sm" name="suffix" id="">
+                    <option value="mr">Mr</option>
+                    <option value="mrs">Mrs</option>
+                </select>
             </div>
+            <div class="vda-group">
+                <label for="name">Customer Surname.</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="Surname...">
+            </div>
+            <div class="vda-group">
+                <label for="name">Customer Firstname.</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="First Name...">
+            </div>
+
+
+            <div class="vda-group">
+                <label for="name">Street Address</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="Street and Number...">
+            </div>
+            <div class="vda-group">
+                <label for="email">Town</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="Town...">
+            </div>
+
+            <div class="vda-group">
+                <label for="name">County</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="County...">
+            </div>
+            <div class="vda-group">
+                <label for="name">Postcode</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="Postcode...">
+            </div>
+
+            <div class="vda-group">
+                <label for="name">Claim No.</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="Claim...">
+            </div>
+            <div class="vda-group">
+                <label for="email">Date of Loss</label>
+                <input class="form-control" type="date" name="date" id="">
+            </div>
+
+            <div class="vda-group">
+                <label for="name">Claim No.</label>
+                <input class="form-control" type="text" name="user" id="" placeholder="Claim...">
+            </div>
+            <div class="vda-group">
+                <label for="email">Date of Loss</label>
+                <input class="form-control" type="date" name="date" id="">
+            </div>
+
+                <button class="btn1 btn-sm">Submit</button>
+            </form>
         </div>
+
+        <div class="col-xxl-4"></div>
+
+    </div>
+</div>
 
 
 <?php
