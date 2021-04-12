b9ab8d2384147a278f29d6e2f132d3c0ef406419
SaracensFan <matthewclifford@hotmail.co.uk>
2021-04-12 11:22:51 +0100

    updates

--- vda-notes.php
+++ vda-notes.php
@@ -5,12 +5,14 @@
 <div class="container-fluid">
     <div class="row">
 
-        <div class="col-sm-4"></div>
+        <div class="col-sm-2"></div>
         
-        <div class="col-sm-4">
+        <div class="col-sm-8">
             <form action="">
-                <div class="group">
-                <h1>Claim Details</h1>
+                    <div class="row">
+                        <h1>Claim Details</h1>
+                    </div>
+                    <div class="group">
                     <div class="vda-group">
                         <label>Repair Instruction Notes</label>
                         <textarea class="form-control" name="1" id="" cols="30" rows="10"></textarea>
@@ -27,7 +29,7 @@
             </form>
         </div>
 
-        <div class="col-sm-4"></div>
+        <div class="col-sm-2"></div>
 
     </div>
 </div>
